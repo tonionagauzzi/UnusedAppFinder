291105fb8bf7fc121813df10fc97488484ecb944
tonionagauzzi <vitantonionagauzzi@gmail.com>
2022-11-08 00:17:04 +0900

    Support global on HowToPermitAppUsage

--- app/src/main/java/com/vitantonio/nagauzzi/unusedappfinder/view/composable/HowToPermitAppUsage.kt
+++ app/src/main/java/com/vitantonio/nagauzzi/unusedappfinder/view/composable/HowToPermitAppUsage.kt
@@ -2,16 +2,22 @@ package com.vitantonio.nagauzzi.unusedappfinder.view.composable
 
 import android.content.Intent
 import android.provider.Settings
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.vitantonio.nagauzzi.unusedappfinder.R
 
 @Composable
@@ -20,21 +26,33 @@ fun HowToPermitAppUsage(
 ) {
     val context = LocalContext.current
 
-    Button(
-        modifier = modifier,
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Transparent,
-            disabledBackgroundColor = Color.Transparent,
-        ),
-        onClick = {
-            context.startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
-        }
+    Column(
+        modifier = modifier.padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        Image(
-            modifier = modifier,
-            contentDescription = stringResource(id = R.string.description_how_to_permit_app_usage),
-            contentScale = ContentScale.Fit,
-            painter = painterResource(id = R.drawable.how_to_permit_app_usage),
+        Text(
+            text = stringResource(id = R.string.description_how_to_permit_app_usage, stringResource(R.string.app_name_short))
         )
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.primary,
+                disabledBackgroundColor = MaterialTheme.colors.primaryVariant,
+            ),
+            onClick = {
+                context.startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            }
+        ) {
+            Text(
+                text = stringResource(id = R.string.button_permit_app_usage),
+                fontSize = 16.sp,
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewHowToPermitAppUsage() {
+    HowToPermitAppUsage(Modifier.fillMaxSize())
 }
