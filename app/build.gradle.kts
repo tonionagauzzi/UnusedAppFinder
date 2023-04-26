7c2ae9d97bbec5dda5fad18bdc7f92c26783ecb4
tonionagauzzi <vitantonionagauzzi@gmail.com>
2023-04-26 23:51:32 +0900

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("kotlin-kapt")
 }
 
+@Suppress("UnstableApiUsage")
 android {
     compileSdk = 33
     defaultConfig {
@@ -27,7 +28,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
     buildTypes {
         named("release") {
@@ -49,23 +50,23 @@ dependencies {
     }
 
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("androidx.activity:activity-compose:1.6.0")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation("androidx.navigation:navigation-compose:2.5.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
+    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.compose.material:material:1.4.2")
+    implementation("androidx.compose.ui:ui:1.4.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.2")
+    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("com.google.accompanist:accompanist-drawablepainter:0.25.1")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.25.1")
-    implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
-    implementation("com.google.dagger:hilt-android:2.44")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.20")
-    kapt("com.google.dagger:hilt-compiler:2.44")
+    implementation("com.google.android.gms:play-services-oss-licenses:17.0.1")
+    implementation("com.google.dagger:hilt-android:2.45")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0")
+    kapt("com.google.dagger:hilt-compiler:2.45")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 kapt {
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -22,9 +22,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        jcenter().mavenContent {
-            includeGroup("com.cookpad.android.licensetools")
-        }
     }
 }
 
