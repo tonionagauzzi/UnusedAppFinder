e1a1ac2d69601310b5d3d12a726674cde5443449
tonionagauzzi <vitantonionagauzzi@gmail.com>
2023-04-27 01:20:31 +0900

    Add LocalDataSource

---
+++ app/src/main/java/com/vitantonio/nagauzzi/unusedappfinder/datasource/AppUsageLocalDataSource.kt
@@ -0,0 +1,69 @@
+package com.vitantonio.nagauzzi.unusedappfinder.datasource
+
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import com.vitantonio.nagauzzi.unusedappfinder.extension.*
+import com.vitantonio.nagauzzi.unusedappfinder.model.AppUsage
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.util.*
+import javax.inject.Inject
+
+class AppUsageLocalDataSource @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
+    fun get(): List<AppUsage> {
+        // Get usage stats list
+        val usageStatsManager =
+            context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val calendar = Calendar.getInstance()
+        val today = Date()
+        val to = today.changeDay(1).resetDateToZeroOClock(calendar).changeMillisecond(-1)
+        val from = Date().changeMonth(-5, calendar).resetDateToStartDayOfMonth(calendar)
+            .resetDateToZeroOClock(calendar)
+        val queryUsageStats = usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_MONTHLY,
+            from.time,
+            to.time
+        )
+        if (queryUsageStats.size == 0) {
+            // Something is wrong
+            throw SecurityException("UsageStatsManager.queryUsageStats() returned empty list.")
+        }
+        // FIXME It seems not to be able to get usage stats before recent shutdown...
+        //val oldestStats = queryUsageStats.filter { it.lastTimeUsed > 1000000000000 }.minBy { it.lastTimeUsed }
+
+        // Get installed app list
+        val packageManager = context.packageManager
+        val mainIntent = Intent(Intent.ACTION_MAIN, null)
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        val resolveInfoList = packageManager.queryIntentActivities(mainIntent, 0)
+
+        // Generate AppUsage list (by usage stats list and installed app list)
+        return resolveInfoList.map { resolveInfo ->
+            AppUsage(
+                name = resolveInfo.loadLabel(packageManager).toString(),
+                packageName = resolveInfo.activityInfo.packageName,
+                activityName = resolveInfo.activityInfo.name,
+                icon = resolveInfo.loadIcon(packageManager),
+                installedTime = packageManager.getPackageInfo(
+                    resolveInfo.activityInfo.packageName,
+                    0
+                ).firstInstallTime,
+                lastUsedTime = queryUsageStats.filter {
+                    it.packageName == resolveInfo.activityInfo.packageName
+                            && it.lastTimeUsed >= 946652400000 // 2000/01/01 00:00:00以降なら正しいデータとみなす
+                }.maxByOrNull {
+                    it.lastTimeUsed
+                }?.lastTimeUsed ?: 0,
+                enableUninstall = resolveInfo.activityInfo.applicationInfo.isUserApp()
+            )
+        }
+    }
+}
+
+private fun ApplicationInfo.isUserApp(): Boolean {
+    val mask = ApplicationInfo.FLAG_SYSTEM or ApplicationInfo.FLAG_UPDATED_SYSTEM_APP
+    return flags and mask == 0
+}
--- app/src/main/java/com/vitantonio/nagauzzi/unusedappfinder/repository/AppUsageRepository.kt
+++ app/src/main/java/com/vitantonio/nagauzzi/unusedappfinder/repository/AppUsageRepository.kt
@@ -1,13 +1,7 @@
 package com.vitantonio.nagauzzi.unusedappfinder.repository
 
-import android.content.Context
 import com.vitantonio.nagauzzi.unusedappfinder.model.AppUsage
-import android.app.usage.UsageStatsManager
-import java.util.*
-import android.content.pm.ApplicationInfo
-import android.content.Intent
-import com.vitantonio.nagauzzi.unusedappfinder.extension.*
-import dagger.hilt.android.qualifiers.ApplicationContext
+import com.vitantonio.nagauzzi.unusedappfinder.datasource.AppUsageLocalDataSource
 import javax.inject.Inject
 
 interface AppUsageRepository {
@@ -15,57 +9,7 @@ interface AppUsageRepository {
 }
 
 class AppUsageRepositoryImpl @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val dataSource: AppUsageLocalDataSource
 ) : AppUsageRepository {
-
-    override fun get(): List<AppUsage> {
-
-        // Get usage stats list
-        val usageStatsManager =
-            context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
-        val calendar = Calendar.getInstance()
-        val today = Date()
-        val to = today.changeDay(1).resetDateToZeroOClock(calendar).changeMillisecond(-1)
-        val from = Date().changeMonth(-5, calendar).resetDateToStartDayOfMonth(calendar).resetDateToZeroOClock(calendar)
-        val queryUsageStats = usageStatsManager.queryUsageStats(
-            UsageStatsManager.INTERVAL_MONTHLY,
-            from.time,
-            to.time
-        )
-        if (queryUsageStats.size == 0) {
-            // Something is wrong
-            throw SecurityException("UsageStatsManager.queryUsageStats() returned empty list.")
-        }
-        // FIXME It seems not to be able to get usage stats before recent shutdown...
-        //val oldestStats = queryUsageStats.filter { it.lastTimeUsed > 1000000000000 }.minBy { it.lastTimeUsed }
-
-        // Get installed app list
-        val packageManager = context.packageManager
-        val mainIntent = Intent(Intent.ACTION_MAIN, null)
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
-        val resolveInfoList = packageManager.queryIntentActivities(mainIntent, 0)
-
-        // Generate AppUsage list (by usage stats list and installed app list)
-        return resolveInfoList.map { resolveInfo ->
-            AppUsage(
-                name = resolveInfo.loadLabel(packageManager).toString(),
-                packageName = resolveInfo.activityInfo.packageName,
-                activityName = resolveInfo.activityInfo.name,
-                icon = resolveInfo.loadIcon(packageManager),
-                installedTime = packageManager.getPackageInfo(resolveInfo.activityInfo.packageName, 0).firstInstallTime,
-                lastUsedTime = queryUsageStats.filter {
-                    it.packageName == resolveInfo.activityInfo.packageName
-                            && it.lastTimeUsed >= 946652400000 // 2000/01/01 00:00:00以降なら正しいデータとみなす
-                }.maxByOrNull {
-                    it.lastTimeUsed
-                }?.lastTimeUsed ?: 0,
-                enableUninstall = resolveInfo.activityInfo.applicationInfo.isUserApp()
-            )
-        }
-    }
-}
-
-private fun ApplicationInfo.isUserApp(): Boolean {
-    val mask = ApplicationInfo.FLAG_SYSTEM or ApplicationInfo.FLAG_UPDATED_SYSTEM_APP
-    return flags and mask == 0
+    override fun get() = dataSource.get()
 }
