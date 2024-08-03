c5ee1920f56d653f0d512b0b1827f28bee6c4c1f
tonionagauzzi <vitantonionagauzzi@gmail.com>
2024-08-04 01:51:18 +0900

    Add screenshot test of night mode

--- app/src/screenshotTest/kotlin/com/vitantonio/nagauzzi/unusedappfinder/ScreenshotTest.kt
+++ app/src/screenshotTest/kotlin/com/vitantonio/nagauzzi/unusedappfinder/ScreenshotTest.kt
@@ -1,5 +1,7 @@
 package com.vitantonio.nagauzzi.unusedappfinder
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.vitantonio.nagauzzi.unusedappfinder.view.composable.PreviewAppUsageTextGroup
@@ -8,19 +10,23 @@ import com.vitantonio.nagauzzi.unusedappfinder.view.composable.PreviewUnusedAppL
 import com.vitantonio.nagauzzi.unusedappfinder.view.composable.PreviewUnusedAppTopBar
 
 class ScreenshotTest {
-    @Preview(showBackground = true)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
     @Composable
     fun AppUsageTextGroupScreenshotTest() = PreviewAppUsageTextGroup()
 
-    @Preview(showBackground = true)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
     @Composable
     fun HowToPermitAppUsageScreenshotTest() = PreviewHowToPermitAppUsage()
 
-    @Preview(showBackground = true)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
     @Composable
     fun UnusedAppListScreenshotTest() = PreviewUnusedAppList()
 
-    @Preview(showBackground = true)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+    @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
     @Composable
     fun UnusedAppTopBarScreenshotTest() = PreviewUnusedAppTopBar()
 }
